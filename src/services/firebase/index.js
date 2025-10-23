30f1d6be9b5528bebe21d2e2fdea0ea6f6cab5c3
EmreEger <m.emre.eger@gmail.com>
2025-10-23 17:45:07 +0300

    todo text güncellendi

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,6 +1,6 @@
 import { addDoc, collection, deleteDoc } from "firebase/firestore";
 import { firebaseDB } from "../../config/firebase";
-import { doc, updateDoc, deleteField } from "firebase/firestore";
+import { doc, setDoc, updateDoc } from "firebase/firestore";
 
 async function addData(tableName, data) {
     // Add a new document in collection "cities"
@@ -18,10 +18,10 @@ async function addData(tableName, data) {
 
 async function deleteData(tableName, docId) {
 
-    const cityRef = doc(firebaseDB, tableName, docId);
+    const todoRef = doc(firebaseDB, tableName, docId);
 
     try {
-        await deleteDoc(cityRef);
+        await deleteDoc(todoRef);
         console.log("OK")
     } catch (error) {
         // ...
@@ -31,7 +31,19 @@ async function deleteData(tableName, docId) {
     }
 }
 
+// async function updateData(tableName, todo, editedTodo) {
+//     const todoEdit = doc(firebaseDB, tableName, todo)
+//     await setDoc(todoEdit, { text: editedTodo });
+// }
+
+const updateData = async (tableName, docId, data) => {
+    const docRef = doc(firebaseDB, tableName, docId);
+    await updateDoc(docRef, data);
+    console.log("Veri güncellendi:", docId);
+};
+
 export {
     addData,
-    deleteData
+    deleteData,
+    updateData
 }
